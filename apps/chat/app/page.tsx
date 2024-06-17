129db4b7c6dd3b30ff811be2e63be157df55dceb
soulofmischief <30357883+soulofmischief@users.noreply.github.com>
2024-06-17 02:07:20 -0700

    Remove root page.tsx

--- apps/chat/app/page.tsx
+++
@@ -1,9 +0,0 @@
-// import { Chat } from '@/components/chat/chat'
-// import { AI } from '@/lib/chat/actions'
-// import { nanoid } from '@/lib/utils'
-// import { getMissingKeys } from '@/app/actions'
-// import { getUser } from '@/utils/supabase/server'
-
-export default async function IndexPage() {
-  return null;
-}
