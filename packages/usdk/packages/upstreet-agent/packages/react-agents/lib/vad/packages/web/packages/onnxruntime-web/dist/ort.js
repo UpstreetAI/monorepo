86dcf7a439aa6bdebda6d38708d31b37686fc6d7
Avaer Kazmer <ultimus@gmail.com>
2024-10-09 18:50:26 -0700

    Add self to ort.js

--- packages/usdk/packages/upstreet-agent/packages/react-agents/lib/vad/packages/web/packages/onnxruntime-web/dist/ort.js
+++ packages/usdk/packages/upstreet-agent/packages/react-agents/lib/vad/packages/web/packages/onnxruntime-web/dist/ort.js
@@ -3,6 +3,7 @@
 * Copyright (c) Microsoft Corporation. All rights reserved.
 * Licensed under the MIT License.
 */
+const self = globalThis;
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
